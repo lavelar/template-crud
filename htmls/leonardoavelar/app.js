cd4dbbc7231ba965be24abd2872860b8a5a8a101
lavelar <leonardo.avelar@aluno.faculdadeimpacta.com.br>
2019-02-28 19:09:07 -0300

    Resolving conflicts

--- htmls/leonardoavelar/app.js
+++ htmls/leonardoavelar/app.js
@@ -1,31 +1,15 @@
-<<<<<<< HEAD
 let App = new Vue({
-=======
-var App = new Vue({
->>>>>>> 34f37a0a1598d4ce1a2545845805ed795d337e00
   el: '#app',
   data: {
     user: '',
     repositorios: []
   },	
-<<<<<<< HEAD
   methods: {
     searchData: () => {
 
-      this.$http.get(`http://api.github.com/users/${App.user}/repos`).then( ({ data }) => {
-        this.repositorios = data
+      App.$http.get(`http://api.github.com/users/${App.user}/repos`).then( ({ data }) => {
+        App.repositorios = data
       })
     }
   }
 })
-=======
-  methods: {    
-  },
-})
-
-function searchData() {
-  App.$http.get(`http://api.github.com/users/${App.user}/repos`).then( ({ data }) => {
-    App.repositorios = data
-  })
-}
->>>>>>> 34f37a0a1598d4ce1a2545845805ed795d337e00
